d377007e8e62a113f786765574f52c40376e02a9
David Wedgbury <david.wedgbury@yahoo.co.uk>
2024-07-23 08:49:40 +0100

    add missing newline

--- Source/Pages/Inflight/InflightPageItemViewModel.cs
+++ Source/Pages/Inflight/InflightPageItemViewModel.cs
@@ -12,6 +12,7 @@ public sealed class InflightPageItemViewModel
     {
         
     }
+
     public InflightPageItemViewModel(MqttApplicationMessage message)
     {
         Message = message ?? throw new ArgumentNullException(nameof(message));
